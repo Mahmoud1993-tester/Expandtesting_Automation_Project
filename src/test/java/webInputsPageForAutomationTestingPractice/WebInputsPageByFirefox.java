1ca36f346289c119ab870dd4ba98a558cb268ba2
Mahmoud Saber <msaber93.egy@gmail.com>
2025-02-13 20:55:10 +0200

    Web Inputs Page commit

---
+++ src/test/java/webInputsPageForAutomationTestingPractice/WebInputsPageByFirefox.java
@@ -0,0 +1,44 @@
+package webInputsPageForAutomationTestingPractice;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class WebInputsPageByFirefox {
+    public static void main(String[] args) throws InterruptedException {
+        // Open the Browser and maximize it
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://practice.expandtesting.com/inputs");
+        driver.manage().window().maximize();
+        // Filling the inputs with valid data
+        driver.findElement(By.id("input-number")).sendKeys("2025");
+        driver.findElement(By.id("input-text")).sendKeys("Free Palestine");
+        driver.findElement(By.id("input-password")).sendKeys("Palestine2025");
+        // Choose the date
+        WebElement dateInput = driver.findElement(By.id("input-date"));
+        // Use JavaScript to set the date value
+        JavascriptExecutor jsDate = (JavascriptExecutor) driver;
+        jsDate.executeScript("arguments[0].value = '1993-09-29';", dateInput);
+        // Scroll into view using JavaScript
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", dateInput);
+        // wait until choosing date
+        Thread.sleep(5000);
+        // Click Display button
+        WebElement displayButton = driver.findElement(By.id("btn-display-inputs"));
+        displayButton.click();
+        // wait data until appear
+        Thread.sleep(5000);
+        // Click Clear button
+        WebElement clearButton = driver.findElement(By.id("btn-clear-inputs"));
+        clearButton.click();
+        // Make the Browser to wait before closing
+        TimeUnit.SECONDS.sleep(5);
+       // Close the Browser
+        driver.close();
+    }
+}
+
